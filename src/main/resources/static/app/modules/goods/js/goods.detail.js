399f775aabd7adf53087ae0e6fae427bc49d0d68
hano-yuwbo <guangqian878@126.com>
2021-06-04 15:53:50 +0800

    加入weui

--- src/main/resources/static/app/modules/goods/js/goods.detail.js
+++ src/main/resources/static/app/modules/goods/js/goods.detail.js
@@ -1,4 +1,4 @@
-angular.module('goods').controller('goodsDetailCtrl', function ($scope) {
+angular.module('goods').controller('goodsDetailCtrl', function ($scope, $stateParams, $sessionStorage) {
     $scope.goods = {
         imageList: [
             {id: 1, src: 'resources/biz/images/yugang.jpg'},
@@ -8,23 +8,48 @@ angular.module('goods').controller('goodsDetailCtrl', function ($scope) {
         stock: 2
     };
     $scope.param = {
-        buyAmount: ''
+        buyAmount: '',
+        addSuccessFlag: false,
+        loadingFlag: false,
     }
     $scope.currentUrl = '';
+    /**
+     * 初始化
+     */
     $scope.onInit = function () {
         $scope.currentUrl = $scope.goods.imageList[0].src;
         $scope.param.buyAmount = 1;
     }
     $scope.onInit();
+    /**
+     * 鼠标指向缩略图时，改变图片SRC
+     * @param idx
+     */
     $scope.overImage = function (idx) {
         $scope.currentUrl = $scope.goods.imageList[idx].src;
     }
+    /**
+     * 购买数量减一
+     */
     $scope.reduceAmount = function () {
         if ($scope.param.buyAmount == 1)return;
         $scope.param.buyAmount = $scope.param.buyAmount - 1;
     }
+    /**
+     * 购买数量加一
+     */
     $scope.plusAmount = function () {
         if ($scope.param.buyAmount >= $scope.goods.stock)return;
         $scope.param.buyAmount = $scope.param.buyAmount + 1;
+    }
+    /**
+     * 加入购物车
+     */
+    $scope.addToCart = function () {
+        if ($("#toast").css('display') != 'none') return;
+        $("#toast").fadeIn(100);
+        setTimeout(function () {
+            $("#toast").fadeOut(100);
+        }, 2000);
     }
 });
